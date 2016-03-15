fa3860dc4cd81e7b9b67430c745a0f6e9b19f65a
Alicia del Sol <adelsol1@uc.cl>
2016-03-15 18:43:31 -0300

    2

--- Tarea1.py
+++ Tarea1.py
@@ -1,1 +1,11 @@
-print "probando"
+# write-html.py
+
+f = open('helloworld.html','w')
+
+message = """<html>
+<head></head>
+<body><p>Hello World!</p></body>
+</html>"""
+
+f.write(message)
+f.close()
